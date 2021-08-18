e4467e605368ed0aba11b80063bddc632c781d5c
dmachard <d.machard@gmail.com>
2021-08-18 09:05:17 +0200

    code factory

---
+++ loggers/fakelogger.go
@@ -0,0 +1,22 @@
+package loggers
+
+import "github.com/dmachard/go-dnscollector/dnsutils"
+
+type FakeLogger struct {
+	channel chan dnsutils.DnsMessage
+}
+
+func NewFakeLogger() *FakeLogger {
+	o := &FakeLogger{
+		channel: make(chan dnsutils.DnsMessage, 512),
+	}
+	return o
+}
+
+func (o *FakeLogger) Stop() {}
+
+func (o *FakeLogger) Channel() chan dnsutils.DnsMessage {
+	return o.channel
+}
+
+func (o *FakeLogger) Run() {}
