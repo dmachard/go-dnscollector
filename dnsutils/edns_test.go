304b8f8df0519dfea62fc64337247cc840986fef
Jukka Taimisto <jtaimisto@gmail.com>
2022-02-18 17:32:14 +0200

    Add basic unit tests for EDNS parsing

--- dnsutils/edns_test.go
+++ dnsutils/edns_test.go
@@ -549,3 +549,205 @@ func TestDecodeAnswer_EdnsErrorShort(t *testing.T) {
 		t.Errorf("bad error returned: %v", erre)
 	}
 }
+
+func TestDecodeEdns_Short(t *testing.T) {
+	testData := []struct {
+		name          string
+		input         []byte
+		expectedError error
+	}{{
+		"empty", []byte{},
+		ErrDecodeDnsLabelTooShort,
+	},
+		{
+			"short", []byte{
+				// empty name
+				0x00,
+			},
+			ErrDecodeDnsAnswerTooShort,
+		},
+		{
+			"invalid rdlength",
+			[]byte{
+				// empty name
+				0x00,
+				// type OPT
+				0x00, 0x29,
+				// class / UDP Payload size
+				0x04, 0xd0,
+				// TTL /  EXT-RCODE=0, VERSION=0, DO=1, Z=0
+				0x00, 0x00, 0x80, 0x00,
+				// RDLENGTH
+				0x00, 0x10,
+				//RDATA
+				// CODE - Extended error
+				0x00, 0x0f,
+				// Length
+				0x00, 0x00,
+				// Option data
+				0x00,
+			},
+			ErrDecodeEdnsDataTooShort,
+		},
+		{
+			"short-option",
+			[]byte{
+				// empty name
+				0x00,
+				// type OPT
+				0x00, 0x29,
+				// class / UDP Payload size
+				0x04, 0xd0,
+				// TTL /  EXT-RCODE=0, VERSION=0, DO=1, Z=0
+				0x00, 0x00, 0x80, 0x00,
+				// RDLENGTH
+				0x00, 0x02,
+				//RDATA
+				// CODE - Extended error
+				0x00, 0x0f,
+			},
+			ErrDecodeEdnsOptionTooShort,
+		},
+		{
+			"invalid-optlen",
+			[]byte{
+				// empty name
+				0x00,
+				// type OPT
+				0x00, 0x29,
+				// class / UDP Payload size
+				0x04, 0xd0,
+				// TTL /  EXT-RCODE=0, VERSION=0, DO=1, Z=0
+				0x00, 0x00, 0x80, 0x00,
+				// RDLENGTH
+				0x00, 0x05,
+				//RDATA
+				// CODE - Extended error
+				0x00, 0x0f,
+				// Length
+				0x00, 0x05,
+				// Option data
+				0x00, 0x00, 0x00,
+			},
+			ErrDecodeEdnsDataTooShort,
+		},
+		{
+			"read-past-rdata",
+			[]byte{
+				// empty name
+				0x00,
+				// type OPT
+				0x00, 0x29,
+				// class / UDP Payload size
+				0x04, 0xd0,
+				// TTL /  EXT-RCODE=0, VERSION=0, DO=1, Z=0
+				0x00, 0x00, 0x80, 0x00,
+				// RDLENGTH
+				0x00, 0x05,
+				//RDATA
+				// CODE - Extended error
+				0x00, 0x0f,
+				// Length
+				0x00, 0x0e,
+				// Option data
+				0x00, 0x00, 0x00,
+				// Another RR starts here, we try to trick the parser
+				// to jum here with invalid option length
+				// empty name
+				0x00,
+				// type OPT
+				0x00, 0x29,
+				// class / UDP Payload size
+				0x04, 0xd0,
+				// TTL /  EXT-RCODE=0, VERSION=0, DO=1, Z=0
+				0x00, 0x00, 0x80, 0x00,
+				// RDLENGTH
+				0x00, 0x07,
+				//RDATA
+				// CODE - Extended error
+				0x00, 0x0f,
+				// Length
+				0x00, 0x03,
+				// Option data
+				0x00, 0x00, 0x00,
+			},
+			ErrDecodeEdnsDataTooShort,
+		},
+		{
+			"invalid-name",
+			[]byte{
+				//  name
+				0x01, 0x61, 0x00,
+				// type OPT
+				0x00, 0x29,
+				// class / UDP Payload size
+				0x04, 0xd0,
+				// TTL /  EXT-RCODE=0, VERSION=0, DO=1, Z=0
+				0x00, 0x00, 0x80, 0x00,
+				// RDLENGTH
+				0x00, 0x06,
+				//RDATA
+				// CODE - Extended error
+				0x00, 0x0f,
+				// Length
+				0x00, 0x03,
+				// Option data
+				0x00, 0x00,
+			},
+			ErrDecodeEdnsBadRootDomain,
+		},
+	}
+
+	for _, test := range testData {
+		t.Run(test.name, func(t *testing.T) {
+			_, _, err := DecodeEDNS(1, 0, test.input)
+			if err != test.expectedError {
+				t.Errorf("bad error: %v", err)
+			}
+		})
+	}
+}
+
+func TestDecodeEdns_MultipleOpts(t *testing.T) {
+	payload := []byte{
+		// empty name
+		0x00,
+		// type OPT
+		0x00, 0x29,
+		// class / UDP Payload size
+		0x04, 0xd0,
+		// TTL /  EXT-RCODE=0, VERSION=0, DO=1, Z=0
+		0x00, 0x00, 0x80, 0x00,
+		// RDLENGTH
+		0x00, 0x06,
+		//RDATA
+		// CODE - Extended error
+		0x00, 0x0f,
+		// Length
+		0x00, 0x02,
+		// Option data
+		0x00, 0x00,
+		// empty name
+		0x00,
+		// type OPT
+		0x00, 0x29,
+		// class / UDP Payload size
+		0x04, 0xd0,
+		// TTL /  EXT-RCODE=0, VERSION=0, DO=0, Z=1
+		0x00, 0x00, 0x00, 0x01,
+		// RDLENGTH
+		0x00, 0x06,
+		//RDATA
+		// CODE - Extended error
+		0x00, 0x0f,
+		// Length
+		0x00, 0x02,
+		// Option data
+		0x00, 0x01,
+	}
+
+	_, _, err := DecodeEDNS(2, 0, payload)
+	if !errors.Is(err, ErrDecodeEdnsTooManyOpts) {
+		t.Errorf("bad error received: %v", err)
+	}
+}
