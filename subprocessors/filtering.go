21a4230e9713fac2055775f1d286752bd59b80a2
Pieter Lexis <pieter.lexis@tomtom.com>
2022-03-02 11:51:28 +0100

    IP filtering: Use subnets and explicit keep
    This allows users to drop certain traffic (like 0.0.0.0/0), but keep
    specific traffic (like 192.0.2.0/25).
    
    Signed-off-by: Pieter Lexis <pieter.lexis@tomtom.com>

--- subprocessors/filtering.go
+++ subprocessors/filtering.go
@@ -10,6 +10,7 @@ import (
 	"github.com/dmachard/go-dnscollector/dnsutils"
 	"github.com/dmachard/go-logger"
 	"gopkg.in/fsnotify.v1"
+	"inet.af/netaddr"
 )
 
 type FilteringProcessor struct {
@@ -17,7 +18,8 @@ type FilteringProcessor struct {
 	logger           *logger.Logger
 	dropDomains      bool
 	mapRcodes        map[string]bool
-	mapQueryIp       map[string]bool
+	ipsetDrop        *netaddr.IPSet
+	ipsetKeep        *netaddr.IPSet
 	listFqdns        map[string]bool
 	listDomainsRegex map[string]*regexp.Regexp
 	fileWatcher      *fsnotify.Watcher
@@ -35,7 +37,8 @@ func NewFilteringProcessor(config *dnsutils.Config, logger *logger.Logger) Filte
 		config:           config,
 		logger:           logger,
 		mapRcodes:        make(map[string]bool),
-		mapQueryIp:       make(map[string]bool),
+		ipsetDrop:        &netaddr.IPSet{},
+		ipsetKeep:        &netaddr.IPSet{},
 		listFqdns:        make(map[string]bool),
 		listDomainsRegex: make(map[string]*regexp.Regexp),
 		fileWatcher:      watcher,
@@ -55,26 +58,59 @@ func (p *FilteringProcessor) LoadRcodes() {
 	}
 }
 
+func (p *FilteringProcessor) loadQueryIpList(fname string, drop bool) (uint64, error) {
+	file, err := os.Open(fname)
+	if err != nil {
+		return 0, err
+	}
+
+	// register the file to watch
+	/*if err := p.fileWatcher.Add(fname); err != nil {
+		p.LogError("unable to watch ip file: ", err)
+	}*/
+
+	scanner := bufio.NewScanner(file)
+	var read uint64
+	var ipsetbuilder netaddr.IPSetBuilder
+	for scanner.Scan() {
+		read++
+		ipOrPrefix := strings.ToLower(scanner.Text())
+		prefix, err := netaddr.ParseIPPrefix(ipOrPrefix)
+		if err != nil {
+			ip, err := netaddr.ParseIP(ipOrPrefix)
+			if err != nil {
+				p.LogError("%s in in %s is neither an IP address nor a prefix", ipOrPrefix, fname)
+				continue
+			}
+			ipsetbuilder.Add(ip)
+			continue
+		}
+		ipsetbuilder.AddPrefix(prefix)
+	}
+	if drop {
+		p.ipsetDrop, err = ipsetbuilder.IPSet()
+	} else {
+		p.ipsetKeep, err = ipsetbuilder.IPSet()
+	}
+
+	return read, err
+}
+
 func (p *FilteringProcessor) LoadQueryIpList() {
 	if len(p.config.Subprocessors.Filtering.DropQueryIpFile) > 0 {
-		file, err := os.Open(p.config.Subprocessors.Filtering.DropQueryIpFile)
+		read, err := p.loadQueryIpList(p.config.Subprocessors.Filtering.DropQueryIpFile, true)
 		if err != nil {
 			p.LogError("unable to open query ip file: ", err)
-		} else {
-
-			// register the file to watch
-			/*if err := p.fileWatcher.Add(p.config.Subprocessors.Filtering.DropQueryIpFile); err != nil {
-				p.LogError("unable to watch ip file: ", err)
-			}*/
-
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				queryip := strings.ToLower(scanner.Text())
-				p.mapQueryIp[queryip] = true
-			}
-			p.LogInfo("loaded with %d query ip to the drop list", len(p.mapQueryIp))
 		}
+		p.LogInfo("loaded with %d query ip to the drop list", read)
+	}
 
+	if len(p.config.Subprocessors.Filtering.KeepQueryIpFile) > 0 {
+		read, err := p.loadQueryIpList(p.config.Subprocessors.Filtering.KeepQueryIpFile, false)
+		if err != nil {
+			p.LogError("unable to open query ip file: ", err)
+		}
+		p.LogInfo("loaded with %d query ip to the keep list", read)
 	}
 }
 
@@ -164,8 +200,12 @@ func (p *FilteringProcessor) CheckIfDrop(dm *dnsutils.DnsMessage) bool {
 		return true
 	}
 
-	// drop according to the query ip ?
-	if _, ok := p.mapQueryIp[dm.NetworkInfo.QueryIp]; ok {
+	// drop or keep according to the query ip ?
+	ip, _ := netaddr.ParseIP(dm.NetworkInfo.QueryIp)
+	if p.ipsetKeep.Contains(ip) {
+		return false
+	}
+	if p.ipsetDrop.Contains(ip) {
 		return true
 	}
 
