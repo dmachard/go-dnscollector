c82b8df1a85a3b15456a2c261d820970fcd956fd
dmachard <5562930+dmachard@users.noreply.github.com>
2024-01-17 21:20:43 +0100

    add test

--- processors/dnstap_test.go
+++ processors/dnstap_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/dmachard/go-dnscollector/dnsutils"
 	"github.com/dmachard/go-dnscollector/loggers"
 	"github.com/dmachard/go-dnscollector/pkgconfig"
 	"github.com/dmachard/go-dnscollector/pkgutils"
@@ -205,4 +206,72 @@ func Test_DnstapProcessor_DisableDNSParser(t *testing.T) {
 	if dm.DNS.ID != 0 {
 		t.Errorf("DNS ID should be equal to zero: %d", dm.DNS.ID)
 	}
+}
+
+func Test_DnstapProcessor_Extended(t *testing.T) {
+	logger := logger.New(true)
+	var o bytes.Buffer
+	logger.SetOutput(&o)
+
+	// init the dnstap consumer
+	cfg := pkgconfig.GetFakeConfig()
+	cfg.Collectors.Dnstap.ExtendedSupport = true
+
+	consumer := NewDNSTapProcessor(0, cfg, logger, "test", 512)
+	// chanTo := make(chan dnsutils.DNSMessage, 512)
+
+	// prepare dns query
+	dnsmsg := new(dns.Msg)
+	dnsmsg.SetQuestion("www.google.fr.", dns.TypeA)
+	dnsquestion, _ := dnsmsg.Pack()
+
+	// prepare dnstap
+	dt := &dnstap.Dnstap{}
+	dt.Type = dnstap.Dnstap_Type.Enum(1)
+
+	dt.Message = &dnstap.Message{}
+	dt.Message.Type = dnstap.Message_Type.Enum(5)
+	dt.Message.QueryMessage = dnsquestion
+
+	edt := &dnsutils.ExtendedDnstap{}
+	edt.Atags = &dnsutils.ExtendedATags{
+		Tags: []string{"atags:value"},
+	}
+	edt.OriginalDnstapExtra = []byte("originalextrafield")
+	edt.Normalize = &dnsutils.ExtendedNormalize{
+		Tld:         "org",
+		EtldPlusOne: "dnscollector.org",
+	}
+	edt.Filtering = &dnsutils.ExtendedFiltering{
+		SampleRate: 30,
+	}
+	edtData, _ := proto.Marshal(edt)
+	dt.Extra = edtData
+
+	data, _ := proto.Marshal(dt)
+
+	// run the consumer with a fake logger
+	fl := loggers.NewFakeLogger()
+	go consumer.Run([]pkgutils.Worker{fl}, []pkgutils.Worker{fl})
+
+	// add packet to consumer
+	consumer.GetChannel() <- data
+
+	// read dns message from dnstap consumer
+	dm := <-fl.GetInputChannel()
+	if dm.DNSTap.Extra != "originalextrafield" {
+		t.Errorf("invalid extra field: %s", dm.DNSTap.Extra)
+	}
+	// if dm.ATags.Tags[0] != "atags:value" {
+	// 	t.Errorf("invalid atags: %s", dm.ATags.Tags[0])
+	// }
+	// if dm.PublicSuffix.QnameEffectiveTLDPlusOne != "dnscollector.org" {
+	// 	t.Errorf("invalid etld+1: %s", dm.PublicSuffix.QnameEffectiveTLDPlusOne)
+	// }
+	// if dm.PublicSuffix.QnamePublicSuffix != "org" {
+	// 	t.Errorf("invalid tld: %s", dm.PublicSuffix.QnamePublicSuffix)
+	// }
+	// if dm.Filtering.SampleRate != 30 {
+	// 	t.Errorf("invalid sample rate: %d", dm.Filtering.SampleRate)
+	// }
 }
