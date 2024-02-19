6e3348051d2f20224a3153a113f14768dac84691
dmachard <5562930+dmachard@users.noreply.github.com>
2024-02-19 10:33:53 +0100

    fix test

--- loggers/fluentd_test.go
+++ loggers/fluentd_test.go
@@ -1,33 +1,41 @@
 package loggers
 
 import (
+	"bytes"
 	"net"
 	"testing"
 	"time"
 
+	"github.com/IBM/fluent-forward-go/fluent/protocol"
 	"github.com/dmachard/go-dnscollector/dnsutils"
 	"github.com/dmachard/go-dnscollector/netlib"
 	"github.com/dmachard/go-dnscollector/pkgconfig"
 	"github.com/dmachard/go-logger"
-	"github.com/vmihailenco/msgpack"
+	"github.com/tinylib/msgp/msgp"
 )
 
 func Test_FluentdClient(t *testing.T) {
 	testcases := []struct {
-		transport string
-		address   string
+		name          string
+		transport     string
+		address       string
+		bufferSize    int
+		flushInterval int
 	}{
 		{
-			transport: netlib.SocketTCP,
-			address:   ":24224",
+			name:          "with_buffer",
+			transport:     netlib.SocketTCP,
+			address:       ":24224",
+			bufferSize:    100,
+			flushInterval: 1,
 		},
 	}
 	for _, tc := range testcases {
-		t.Run(tc.transport, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			// init logger
 			cfg := pkgconfig.GetFakeConfig()
-			cfg.Loggers.Fluentd.FlushInterval = 1
-			cfg.Loggers.Fluentd.BufferSize = 0
+			cfg.Loggers.Fluentd.FlushInterval = tc.flushInterval
+			cfg.Loggers.Fluentd.BufferSize = tc.bufferSize
 			g := NewFluentdClient(cfg, logger.New(false), "test")
 
 			// fake msgpack receiver
@@ -46,27 +54,73 @@ func Test_FluentdClient(t *testing.T) {
 				return
 			}
 			defer conn.Close()
+			time.Sleep(time.Second)
 
 			// send fake dns message to logger
-			time.Sleep(time.Second)
 			dm := dnsutils.GetFakeDNSMessage()
-			g.GetInputChannel() <- dm
+			maxDm := 256
+			for i := 0; i < maxDm; i++ {
+				g.GetInputChannel() <- dm
+			}
+			time.Sleep(time.Second)
 
 			// read data on fake server side
-			buf := make([]byte, 4096)
-			_, err = conn.Read(buf)
-			if err != nil {
-				t.Errorf("error to read msgpack: %s", err)
+			nb := 0
+			bytesSize := 0
+			conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+			fullBuffer := make([]byte, 0)
+			for {
+				buf := make([]byte, 4096)
+				n, _ := conn.Read(buf)
+				if n == 0 {
+					break
+				}
+				bytesSize += n
+				fullBuffer = append(fullBuffer, buf[:n]...)
 			}
 
-			// unpack msgpack
-			var dmRcv dnsutils.DNSMessage
-			err = msgpack.Unmarshal(buf[24:], &dmRcv)
-			if err != nil {
-				t.Errorf("error to unpack msgpack: %s", err)
+			msgpr := msgp.NewReader(bytes.NewReader(fullBuffer[:bytesSize]))
+
+			for {
+				sz, err := msgpr.ReadArrayHeader()
+				if err != nil {
+					t.Errorf("decode Array Header failed: %v", err)
+					break
+				}
+				if sz != 3 {
+					t.Errorf("decode expect 3 elements: %d", sz)
+					break
+				}
+				tag, err := msgpr.ReadString()
+				if err != nil {
+					t.Errorf("Decode tag: %v", err)
+					break
+				}
+				if tag != "dns.collector" {
+					t.Errorf("invalid tag: %s", tag)
+					break
+				}
+
+				entries := protocol.EntryList{}
+				if err = entries.DecodeMsg(msgpr); err != nil {
+					t.Errorf("decode Entries: %v", err)
+					break
+				}
+				nb += len(entries)
+
+				options := &protocol.MessageOptions{}
+				if err = options.DecodeMsg(msgpr); err != nil {
+					t.Errorf("decode options: %v", err)
+					break
+				}
+
+				if msgpr.Buffered() == 0 {
+					break
+				}
 			}
-			if dm.DNS.Qname != dmRcv.DNS.Qname {
-				t.Errorf("qname error want %s, got %s", dm.DNS.Qname, dmRcv.DNS.Qname)
+
+			if nb != maxDm {
+				t.Errorf("invalid numer of msgpack: expected=%d received=%d", maxDm, nb)
 			}
 
 			// stop all
