1dde5cc7ebdd552c89d8a87981161c8dd06d8af7
Denis Machard <5562930+dmachard@users.noreply.github.com>
2024-05-07 21:24:36 +0200

    fix regression to detect logger in pipeline mode (#701)

--- pkglinker/pipelines.go
+++ pkglinker/pipelines.go
@@ -19,6 +19,13 @@ func GetStanzaConfig(config *pkgconfig.Config, item pkgconfig.ConfigPipelines) *
 
 	// Enable the provided collector or loggers
 	for k, p := range item.Params {
+		// is a logger or collector ?
+		if !config.Loggers.IsExists(k) && !config.Collectors.IsExists(k) {
+			panic(fmt.Sprintln("main - get stanza config error"))
+		}
+		if config.Loggers.IsExists(k) {
+			section = "loggers"
+		}
 		if p == nil {
 			item.Params[k] = make(map[string]interface{})
 		}
