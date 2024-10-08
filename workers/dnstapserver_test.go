12fbd5af3453279d654db6fe5994cc0e096285b1
dmachard <5562930+dmachard@users.noreply.github.com>
2024-10-08 20:35:09 +0200

    fix: invalid ingress counter for dnstap collector

--- workers/dnstapserver_test.go
+++ workers/dnstapserver_test.go
@@ -240,7 +240,7 @@ func Test_DnstapProcessor_toDNSMessage(t *testing.T) {
 	}
 }
 
-func Test_DnstapProcessor_DecodeCounters(t *testing.T) {
+func Test_DnstapProcessor_DecodeDNSCounters(t *testing.T) {
 	logger := logger.New(true)
 	var o bytes.Buffer
 	logger.SetOutput(&o)
@@ -620,5 +620,46 @@ func Test_DnstapProcessor_BufferLoggerIsFull(t *testing.T) {
 	dm2 := <-fl.GetInputChannel()
 	if dm2.DNS.Qname != pkgconfig.ExpectedQname {
 		t.Errorf("invalid qname in second dns message: %s", dm2.DNS.Qname)
+	}
+}
+
+// test for telemetry counter
+func Test_DnstapProcessor_TelemetryCounters(t *testing.T) {
+	logger := logger.New(true)
+	var o bytes.Buffer
+	logger.SetOutput(&o)
+
+	// run the consumer with a fake logger
+	fl := GetWorkerForTest(pkgconfig.DefaultBufferSize)
+
+	// init the dnstap consumer
+	consumer := NewDNSTapProcessor(0, "peertest", pkgconfig.GetDefaultConfig(), logger, "test", 512)
+	consumer.AddDefaultRoute(fl)
+	consumer.AddDroppedRoute(fl)
+
+	// get dns packet
+	queryPkt, _ := dnsutils.GetFakeDNS()
+
+	// prepare dnstap
+	dt := &dnstap.Dnstap{}
+	dt.Type = dnstap.Dnstap_Type.Enum(1)
+
+	dt.Message = &dnstap.Message{}
+	dt.Message.Type = dnstap.Message_Type.Enum(5) // CLIENT_QUERY
+	dt.Message.ResponseMessage = queryPkt
+	data, _ := proto.Marshal(dt)
+
+	// start the consumer
+	go consumer.StartCollect()
+
+	// add packet to consumer and read output
+	consumer.GetDataChannel() <- data
+	<-fl.GetInputChannel()
+
+	if consumer.totalIngress != 1 {
+		t.Errorf("invalid total ingress oucnter: got %d expect 1", consumer.totalIngress)
+	}
+	if consumer.totalEgress != 1 {
+		t.Errorf("invalid total egress counter: got %d expect 1", consumer.totalEgress)
 	}
 }
